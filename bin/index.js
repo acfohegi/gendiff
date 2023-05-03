d58c55b442ec8801ed4d90a4fb7587f6db069d7e
Виктор Бакеев <vbakeev@rnds.pro>
2023-05-04 01:59:17 +0400

    8: fix default format for main

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@ import format from '../src/formatters/index.js';
 import differ from '../src/differ.js';
 import parse from '../src/parsers.js';
 
-export default (filepath1, filepath2, userFormat) => {
+export default (filepath1, filepath2, userFormat = 'stylish') => {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
   const diff = differ(data1, data2);
